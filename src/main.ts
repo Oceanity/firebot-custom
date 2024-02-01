95180a446832c392afe8d9558dffaa1b3d97ca22
Oceanity <github@oceanity.net>
2024-01-31 21:43:37 -0500

    Updated Script Manifest

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,33 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
 
 interface Params {
-  message: string;
+    message: string;
 }
 
 const script: Firebot.CustomScript<Params> = {
-  getScriptManifest: () => {
-    return {
-      name: "Starter Custom Script",
-      description: "A starter custom script for build",
-      author: "SomeDev",
-      version: "1.0",
-      firebotVersion: "5",
-    };
-  },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
-  run: (runRequest) => {
-    const { logger } = runRequest.modules;
-    logger.info(runRequest.parameters.message);
-  },
+    getScriptManifest: () => {
+        return {
+            name: "Oshi's Custom Firebot Startup",
+            description: "Extends Firebot functionality",
+            author: "Oceanity",
+            version: "1.0",
+            firebotVersion: "5",
+        };
+    },
+    getDefaultParameters: () => {
+        return {
+            message: {
+                type: "string",
+                default: "Hello World!",
+                description: "Message",
+                secondaryDescription: "Enter a message here",
+            },
+        };
+    },
+    run: (runRequest) => {
+        const { logger } = runRequest.modules;
+        logger.info(runRequest.parameters.message);
+    },
 };
 
 export default script;
