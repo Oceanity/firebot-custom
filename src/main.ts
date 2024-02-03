8486d0fe34f3b13b84c278c8a634813909355071
Oceanity <github@oceanity.net>
2024-02-02 19:20:01 -0500

    Updated Path Aliases

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,33 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
-import useRoutes from "@/router";
+import useRouter from "@/router";
 
 interface Params {
   message: string;
 }
 
 const script: Firebot.CustomScript<Params> = {
-  getScriptManifest: () => {
-    return {
-      name: "Oshi's Custom Firebot Startup",
-      description: "Extends Firebot functionality",
-      author: "Oceanity",
-      version: "1.0",
-      firebotVersion: "5",
-    };
-  },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
+  getScriptManifest: () => ({
+    name: "Oshi's Custom Firebot Startup",
+    description: "Extends Firebot functionality",
+    author: "Oceanity",
+    version: "1.0",
+    firebotVersion: "5",
+  }),
+  getDefaultParameters: () => ({
+    message: {
+      type: "string",
+      default: "Hello World!",
+      description: "Message",
+      secondaryDescription: "Enter a message here",
+    },
+    test: {
+      type: "button",
+    },
+  }),
   run: (runRequest) => {
-    const { logger } = runRequest.modules;
+    const { logger, httpServer } = runRequest.modules;
     logger.info(runRequest.parameters.message);
-    useRoutes();
+    useRouter(httpServer);
   },
 };
 
