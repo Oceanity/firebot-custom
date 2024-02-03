eb277fa2d8b6951cd9957e91cac72cb737997773
Oceanity <github@oceanity.net>
2024-02-03 14:04:35 -0500

    Feature/setup endpoints (#9)
    * Added `router` skeleton, made `.prettierrc` cover everything with `tabWidth` rule because why not, linked `@/` to `/src/`
    
    * Changed `routes` to function
    
    * Added `useRoutes()` to script entry point
    
    * Updated Path Aliases
    
    * Tried getting Aliases working
    
    * Added `router` skeleton, made `.prettierrc` cover everything with `tabWidth` rule because why not, linked `@/` to `/src/`
    
    * Changed `routes` to function
    
    * Added `useRoutes()` to script entry point
    
    * Updated Path Aliases
    
    * Tried getting Aliases working
    
    * Removed duplicate key from merge

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,33 @@
 import { Firebot } from "@crowbartools/firebot-custom-scripts-types";
+import useRouter from "@/router";
 
 interface Params {
   message: string;
 }
 
 const script: Firebot.CustomScript<Params> = {
-  getScriptManifest: () => {
-    return {
-      name: "Oshi's Custom Firebot Startup",
-      description: "Extends Firebot functionality",
-      author: "Oceanity",
-      version: "1.0",
-      firebotVersion: "5",
-    };
-  },
-  getDefaultParameters: () => {
-    return {
-      message: {
-        type: "string",
-        default: "Hello World!",
-        description: "Message",
-        secondaryDescription: "Enter a message here",
-      },
-    };
-  },
+  getScriptManifest: () => ({
+    name: "Oshi's Custom Firebot Startup",
+    description: "Extends Firebot functionality",
+    author: "Oceanity",
+    version: "1.0",
+    firebotVersion: "5",
+  }),
+  getDefaultParameters: () => ({
+    message: {
+      type: "string",
+      default: "Hello World!",
+      description: "Message",
+      secondaryDescription: "Enter a message here",
+    },
+    test: {
+      type: "button",
+    },
+  }),
   run: (runRequest) => {
-    const { logger } = runRequest.modules;
+    const { logger, httpServer } = runRequest.modules;
     logger.info(runRequest.parameters.message);
+    useRouter(httpServer);
   },
 };
 
