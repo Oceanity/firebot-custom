22993c6171ae215a99abcff04b1c1aa766970932
Oceanity <github@oceanity.net>
2024-02-15 16:37:09 -0500

    Update Copy Build flow (#11)

--- scripts/copy-build.js
+++ scripts/copy-build.js
@@ -1,33 +1,33 @@
 /**
  *  Copies the built script .js to Firebot's scripts folder
  */
-const fs = require("fs").promises;
-const { Console } = require("console");
+const fs = require("fs-extra");
 const path = require("path");
+const { scriptOutputName } = require("../package.json");
 
-const extractScriptName = () => {
-  const packageJson = require("../package.json");
-  return `${packageJson.scriptOutputName}.js`;
-};
+const getAppDataFolderPath = () => {
+  switch (process.platform) {
+    case "win32":
+      return process.env.APPDATA;
+    case "darwin":
+      return path.join(
+        process.env.HOME,
+        "/Library/Application Support"
+      );
+    case "linux":
+      return path.join(
+        process.env.HOME,
+        "/.config"
+      );
+    default:
+      return null;
+  }
+}
 
 const getFirebotScriptsFolderPath = () => {
   // determine os app data folder
-  let appDataFolderPath;
-  if (process.platform === "win32") {
-    appDataFolderPath = process.env.APPDATA;
-  } else if (process.platform === "darwin") {
-    appDataFolderPath = path.join(
-      process.env.HOME,
-      "/Library/Application Support"
-    );
-  } else if (process.platform === "linux") {
-    appDataFolderPath = path.join(
-      process.env.HOME,
-      "/.config"
-    );
-  } else {
-    throw new Error("Unsupported OS!");
-  }
+  const appDataFolderPath = getAppDataFolderPath();
+  if (!appDataFolderPath) throw new Error("Unsupported OS!");
 
   const firebotDataFolderPath = path.join(appDataFolderPath, "/Firebot/v5/");
   const firebotGlobalSettings = require(path.join(
@@ -54,18 +54,19 @@ const getFirebotScriptsFolderPath = () => {
 
 const main = async () => {
   const firebotScriptsFolderPath = getFirebotScriptsFolderPath();
+  const scriptFileName = `${scriptOutputName}.js`
+  const outputDir = `${firebotScriptsFolderPath}/${scriptOutputName}/`;
 
-  const scriptName = extractScriptName();
-
-  const srcScriptFilePath = path.resolve(`./dist/${scriptName}`);
+  const srcScriptFilePath = path.resolve(`./dist/${scriptFileName}`);
   const destScriptFilePath = path.join(
-    firebotScriptsFolderPath,
-    `${scriptName}`
+    outputDir,
+    "index.js"
   );
 
+  await fs.ensureDir(outputDir);
   await fs.copyFile(srcScriptFilePath, destScriptFilePath);
 
-  console.log(`Successfully copied ${scriptName} to Firebot scripts folder.`);
+  console.log(`Successfully copied ${scriptFileName} to Firebot scripts folder.`);
 };
 
 main();
