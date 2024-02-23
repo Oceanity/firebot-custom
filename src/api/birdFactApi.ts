5ce81e074cbde5c22ab252e165cce3a21ee80244
Oceanity <github@oceanity.net>
2024-02-22 23:06:31 -0500

    Removed unused import

--- src/api/birdFactApi.ts
+++ src/api/birdFactApi.ts
@@ -1,4 +1,3 @@
-import NuthatchUtils from "@/utils/nuthatchUtils";
 import { Request, Response } from "express";
 import { getRequestDataFromUri } from "@/utils/requestUtils";
 import BirdFactUtils from "@/utils/birdFactUtils";
@@ -6,19 +5,15 @@ import store from "@u/global";
 
 export default class BirdFactApi {
   private static readonly route: string= "/birdFacts";
-
   private readonly birdFactUtils: BirdFactUtils;
-  private readonly nuthatchUtils: NuthatchUtils;
 
   constructor() {
     this.birdFactUtils = new BirdFactUtils();
-    this.nuthatchUtils = new NuthatchUtils();
   }
 
   setup = async () => {
     store.modules.logger.info("Setting up BirdFactApi...");
     await this.birdFactUtils.setup();
-    await this.nuthatchUtils.setup();
     await this.registerEndpoints();
   }
 
@@ -28,21 +23,23 @@ export default class BirdFactApi {
     const { httpServer } = modules;
 
     // Birb Fact Endpoints
-    httpServer.registerCustomRoute(prefix, route, "GET", this.getBirdFactHandler);
-    httpServer.registerCustomRoute(prefix, `${route}/all`, "GET", this.getAllBirdFactsHandler);
-    httpServer.registerCustomRoute(prefix, route, "PUT", this.putNewBirdFactHandler);
+    httpServer.registerCustomRoute(prefix, route, "GET", this.getHandler);
+    httpServer.registerCustomRoute(prefix, `${route}/all`, "GET", this.getAllHandler);
+    httpServer.registerCustomRoute(prefix, route, "PUT", this.putHandler);
   }
 
-  //#region Bird Fact Handlers
-  private getBirdFactHandler = async (req: Request, res: Response) => {
+  //#region Endpoints Handlers
+  private getHandler = async (req: Request, res: Response): Promise<void> => {
     const { id } = getRequestDataFromUri(req.url).params;
     res.send(await this.birdFactUtils.getBirdFact(parseInt(id)));
   }
 
-  private getAllBirdFactsHandler = async (req: Request, res: Response) =>
+  private getAllHandler = async (req: Request, res: Response): Promise<void> => {
     res.send(await this.birdFactUtils.getAllBirdFacts());
+  }
 
-  private putNewBirdFactHandler = async (req: Request, res: Response) =>
+  private putHandler = async (req: Request, res: Response): Promise<void> => {
     res.send(await this.birdFactUtils.putBirdFact());
+  }
   //#endregion
 }
